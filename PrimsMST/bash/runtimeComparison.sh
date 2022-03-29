042607a9c7a99985af5f352f03c71a7bab7a087c
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2022-03-29 11:14:49 -0400

    Runtime calculation adjustment & first benchmark

--- PrimsMST/bash/runtimeComparison.sh
+++ PrimsMST/bash/runtimeComparison.sh
@@ -4,19 +4,14 @@
 
 g++ $1 -std=c++17 -o seq.out
 g++ -Xpreprocessor -fopenmp $2 -std=c++17 -lomp -o par.out
-
+TIMEFORMAT=%R
 for i in {1..10}
 do  
-    start_time_seq=$(date +%s)
-    ./seq.out < ../input/in$i.in > temp_seq.out
-    end_time_seq=$(date +%s)
-    elapsed_seq=$(($end_time_seq - $start_time_seq))
-    start_time_par=$(date +%s)
-    echo $elapsed_seq "s elapsed -- sequential"
-    ./par.out < ../input/in$i.in > temp_par.out
-    end_time_par=$(date +%s)
-    elapsed_par=$(($end_time_par - $start_time_par))
-    echo $elapsed_par "s elapsed -- parallel"
+    echo "Input $i"
+    time ./seq.out < ../input/in$i.in > temp_seq.out
+    # echo "sequential"
+    time ./par.out < ../input/in$i.in > temp_par.out
+    # echo "parallel"
     diff -b temp_seq.out temp_par.out > diffed.out
     [ -s diffed.out ] && echo "Wrong output test $i"
 done
