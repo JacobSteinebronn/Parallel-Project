9903752ae3b4777e0754444df24aaa7831976be0
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2022-04-16 11:04:00 -0400

    prim_par simplification

--- report/src/MST/prims_par.cpp
+++ report/src/MST/prims_par.cpp
@@ -1,20 +1,5 @@
-void primMST(int** graph)
-{
-	for (int i = 0; i < num_nodes; i++)
-		key[i] = INT_MAX, mstSet[i] = false;
-
-	key[0] = 0;
-	parent[0] = -1; 
-	for (int count = 0; count < num_nodes - 1; count++)
-	{
-		int u = minKey(key, mstSet);
-		// Add the picked vertex to the MST Set
-		mstSet[u] = true;
-		// Set parallelization point 
 #pragma omp parallel for schedule(static)
-		for (int v = 0; v < num_nodes; v++)
-			// Update the key only if graph[u][v] is smaller than key[v]
-			if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-				parent[v] = u, key[v] = graph[u][v];
-	}
-}
+    for (int v = 0; v < num_nodes; v++)
+        // Update the key only if graph[u][v] is smaller than key[v]
+        if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
+            parent[v] = u, key[v] = graph[u][v];
